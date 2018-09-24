a66dcfa8ccfb86c4bfe935975dd63e4e70eb03e8
Ciro S. Costa <ciro9758@gmail.com>
2018-09-24 23:02:49 +0000

    Adds linux implementation

--- pidpath/pidpath_linux.go
+++ pidpath/pidpath_linux.go
@@ -1,6 +1,12 @@
 // +build linux
 package pidpath
 
+import (
+	"os"
+	"strconv"
+)
+
 func GetExePathFromPid(pid int) (path string, err error) {
+	path, err = os.Readlink("/proc/" + strconv.Itoa(pid) + "/exe")
 	return
 }
