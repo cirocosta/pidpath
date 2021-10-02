cb3326cfffec219e1cddbafa4173cd73bd7cee3a
Crow <5488190+echocrow@users.noreply.github.com>
2021-10-02 23:43:04 +0200

    Add basic test

---
+++ pidpath_test.go
@@ -0,0 +1,38 @@
+package pidpath_test
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/echocrow/pidpath"
+)
+
+func TestGet(t *testing.T) {
+	pid := int32(os.Getpid())
+	if pid == 0 {
+		t.Fatalf("Failed to retrieve current PID")
+	}
+
+	realPath, err := os.Executable()
+	if err == nil {
+		// os.Executable() may return a symlink'd path.
+		realPath, err = filepath.EvalSymlinks(realPath)
+	}
+	if err != nil {
+		t.Fatalf("Failed to retrieve current executable: %v", err)
+	}
+
+	gotPath, err := pidpath.Get(pid)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v", err)
+	}
+
+	if gotPath != realPath {
+		t.Fatalf(
+			"Expected no get executable path.\nwant: %s\ngot: %s",
+			realPath,
+			gotPath,
+		)
+	}
+}
